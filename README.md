43851d9aa4469f09609aba555de3855b4f836ec1
Glitch (mapsaw) <none>
2019-12-31 18:58:36 +0000

    Updated with Glitch

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ node express pug
 
 mapsaw map tiles from maptiler nasa https://tileserver.maptiler.com/nasa/
 
+edit .ENV 
+
+add MAPTILE_KEY=yourkey
+
 Refs
 
 https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API
--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 
-const key = process.env.AIRVISUAL_KEY;
-const city = process.env.AIRVISUAL_CITY_NAME;
-const state = process.env.AIRVISUAL_STATE_NAME;
-const country = process.env.AIRVISUAL_COUNTRY_NAME;
+const maptilekey = process.env.MAPTILE_KEY;
 
 const app = express();
 
@@ -20,58 +17,55 @@ app.get("/", async (_, res) => {
 });
 
 async function getAqi() {
-  const params = { key, city, state, country };
-  let aqi = {
-    value: 0,
-    level: "",
-    message: ""
-  };
+  let aqi = {};
+
+  var key = "?key=" + maptilekey;
 
   try {
-    var img = "https://tileserver.maptiler.com/nasa/";
-
-    var z = 3;
-    aqi.images1 = [
-      [
-        img + z + "/1/1.png",
-        img + z + "/2/1.png",
-        img + z + "/3/1.png",
-        img + z + "/4/1.png"
-      ],
-
-      [
-        img + z + "/1/2.png",
-        img + z + "/2/2.png",
-        img + z + "/3/2.png",
-        img + z + "/4/2.png"
-      ],
-
-      [
-        img + z + "/1/3.png",
-        img + z + "/2/3.png",
-        img + z + "/3/3.png",
-        img + z + "/4/3.png"
-      ],
-
-      [
-        img + z + "/1/4.png",
-        img + z + "/2/4.png",
-        img + z + "/3/4.png",
-        img + z + "/4/4.png"
-      ]
+    var maptiles = [
+      ["https://tileserver.maptiler.com/nasa/", 3, 1, 1, ".png"],
+      ["https://tileserver.maptiler.com/nasa/", 4, 3, 7, ".png"],
+      ["https://tileserver.maptiler.com/nasa/", 5, 8, 14, ".png"],
+      ["https://api.maptiler.com/tiles/satellite/", 5, 8, 14, ".jpg" + key],
+      ["https://api.maptiler.com/tiles/satellite/", 6, 12, 14, ".jpg" + key],
+      ["https://api.maptiler.com/tiles/satellite/", 6, 18, 33, ".jpg" + key]
+      ["https://api.maptiler.com/tiles/satellite/", 12, 1986, 3474, ".jpg" + key],
     ];
 
+    var svr = maptiles[Math.floor(Math.random() * (maptiles.length - 1))];
+
+    var prefix = svr[0];
+    var z = svr[1];
+    var top = svr[2];
+    var left = svr[3];
+    var suffix = svr[4];
+
+    aqi.images11 = [];
+
+    var table = [];
+    for (var r = 0; r < 4; r++) {
+      var row = [];
+      var rr = top + r;
+      for (var c = 0; c < 4; c++) {
+        var cc = left + c;
+        row.push(prefix + z + "/" + cc + "/" + rr + suffix);
+      }
+      table.push(row);
+    }
+
+    aqi.images1 = table;
+
     aqi.images2 = [];
-    var images3 = [];
+    row = [];
 
     for (var i = 0; i < 4; i++) {
       var x = Math.floor(Math.random() * 4);
       var y = Math.floor(Math.random() * 4);
-      images3.push(aqi.images1[x][y]);
+      row.push(aqi.images1[x][y]);
       aqi.images1[x][y] = "";
     }
 
-    aqi.images2.push(images3);
+    aqi.images2.push(row);
 
     return aqi;
   } catch (error) {

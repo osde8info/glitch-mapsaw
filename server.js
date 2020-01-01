e7e30f9e59dac6d6c018effc2db992fb4f79ff0f
Glitch (mapsaw) <none>
2020-01-01 16:46:09 +0000

    add h & w

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.get("/", async (_, res) => {
 async function getAqi() {
   let aqi = {};
 
+  const width=5
+  const height=4
+  
   var key = "?key=" + maptilekey;
 
   try {
@@ -26,10 +29,9 @@ async function getAqi() {
       ["https://tileserver.maptiler.com/nasa/", 3, 1, 1, ".png"],
       ["https://tileserver.maptiler.com/nasa/", 4, 3, 7, ".png"],
       ["https://tileserver.maptiler.com/nasa/", 5, 8, 14, ".png"],
-      ["https://api.maptiler.com/tiles/satellite/", 5, 8, 14, ".jpg" + key],
       ["https://api.maptiler.com/tiles/satellite/", 6, 12, 14, ".jpg" + key],
-      ["https://api.maptiler.com/tiles/satellite/", 6, 18, 33, ".jpg" + key],
-      ["https://api.maptiler.com/tiles/satellite/", 12, 1986, 3474, ".jpg" + key],
+      ["https://api.maptiler.com/tiles/satellite/", 8, 87, 132, ".jpg" + key],
+      ["https://api.maptiler.com/tiles/satellite/", 12, 1396, 2116, ".jpg" + key],
     ];
 
     var svr = maptiles[Math.floor(Math.random() * maptiles.length)];
@@ -43,10 +45,10 @@ async function getAqi() {
     aqi.images11 = [];
 
     var table = [];
-    for (var r = 0; r < 4; r++) {
+    for (var r = 0; r < height; r++) {
       var row = [];
       var rr = top + r;
-      for (var c = 0; c < 4; c++) {
+      for (var c = 0; c < width; c++) {
         var cc = left + c;
         row.push(prefix + z + "/" + cc + "/" + rr + suffix);
       }
@@ -58,11 +60,11 @@ async function getAqi() {
     aqi.images2 = [];
     row = [];
 
-    for (var i = 0; i < 4; i++) {
-      var x = Math.floor(Math.random() * 4);
-      var y = Math.floor(Math.random() * 4);
-      row.push(aqi.images1[x][y]);
-      aqi.images1[x][y] = "";
+    for (var i = 0; i < width; i++) {
+      var x = Math.floor(Math.random() * width);
+      var y = Math.floor(Math.random() * height);
+      row.push(aqi.images1[y][x]);
+      aqi.images1[y][x] = "";
     }
 
     aqi.images2.push(row);

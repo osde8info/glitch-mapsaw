24391ddeb6bcb147bd122ccd297d61343451fe37
Clive Darra <osde8info@users.noreply.github.com>
2020-01-01 16:03:59 +0000

    Update longlattozxy.js

--- longlattozxy.js
+++ longlattozxy.js
@@ -1,5 +1,10 @@
-function long2tile(lon,zoom) { return (Math.floor((lon+180)/360*Math.pow(2,zoom))); }
+function lat2tile(lat,zoom)  { return (Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) 
+                                                              + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom))) }
 
-function lat2tile(lat,zoom)  { return (Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom))); }
+function long2tile(lon,zoom) { return (Math.floor((lon+180)/360*Math.pow(2,zoom))) }
 
-console.log(long2tile(51.1,12)+' '+lat2tile(0.1,12)                     )
+function getlatlong(zoom,lat,lon) {
+  console.log(zoom+','+lat2tile(lat,zoom) +','+long2tile(lon,zoom))
+}
+
+getlatlong(14,51.0,0.1)
